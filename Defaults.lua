302a332821897188b360c62f5223520815aa9624
Argi <15852038+argium@users.noreply.github.com>
2026-02-21 18:22:28 +1100

    revert update interval change.

--- Defaults.lua
+++ Defaults.lua
@@ -127,7 +127,7 @@ local defaults = {
         global = {
             hideWhenMounted = true,
             hideOutOfCombatInRestAreas = false,
-            updateFrequency = 0.08,
+            updateFrequency = 0.04,
             barHeight = 22,
             barBgColor = { r = 0.08, g = 0.08, b = 0.08, a = 0.75 },
             offsetY = 4,
